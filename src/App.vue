6f7ede88018cade5615b12fcc0020652fba0be07
l.shuang <lsbright@qq.com>
2021-09-13 16:36:33 +0800

    reactive的基本使用，操作代理对象影响界面更新渲染

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,62 @@
 <template>
-  <h2>setup和ref的基本使用</h2>
-  <h3>{{ count }}</h3>
-  <button @click="updateCount">更新数据</button>
+  <h2>reactive的使用</h2>
+  <h3>名字：{{ user.name }}</h3>
+  <h3>年龄：{{ user.age }}</h3>
+  <h3>性别：{{ user.gender }}</h3>
+  <h3>媳妇：{{ user.wife }}</h3>
+  <hr>
+  <button @click="updateUser">更新数据</button>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, reactive } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  // 需求：页面打开后看到一个数据，点击按钮，数据发生变化
-
-  // vue2实现方式
-  // data() {
-  //   return {
-  //     count: 0,
-  //   }
-  // },
-  // methods: {
-  //   updateCount() {
-  //     this.count ++
-  //   }
-  // }
-
-  // vue3实现方式
-  // setup是组合API的入口函数
+  // 需求： 显示用户的相关信息，点击按钮可以更新
+  /**
+   * 作用: 定义多个数据的响应式
+   * const proxy = reactive(obj): 接收一个普通对象然后返回该普通对象的响应式代理器对象
+   * 响应式转换是“深层的”：会影响对象内部所有嵌套的属性
+   * 内部基于 ES6 的 Proxy 实现，通过代理对象操作源对象内部数据都是响应式的
+   */
   setup() {
-    // let count = 0 // 此时的数据并不是响应式的
-    // ref作用：定义一个响应式数据，返回一个reference（Ref）对象，其中有一个value属性，如果需要对数据进行操作，需要通过该Ref对象调用value的方式进行操作
-    // html模板中不需要.value写法
-    // ref一般用来定义一个基本数据类型的响应式数据
-    const count = ref(0)
-    console.log(count)
+    const obj = {
+      name: '小明',
+      age: 20,
+      wife: {
+        name: '小甜甜',
+        age: 18,
+        cars: ['奔驰', '宝马', '奥迪']
+      }
+    }
+    // 把数据变成响应式的数据
+    // 返回的是一个Proxy的代理对象，被代理的对象就是obj对象
+    // user现在是代理对象，obj是目标对象
+    // user对象的类型是Proxy
+    const user = reactive(obj)
 
-    function updateCount() {
-      count.value ++
+    console.log(user)
+
+    const updateUser = () => {
+      user.name += '=='
+      user.age += 2
+      user.wife.name += '=='
+      user.wife.cars[0] = '玛莎拉蒂'
+
+      // user-->代理对象，obj-->目标对象
+      // user对象或者obj对象添加一个新属性，哪种方式会影响界面更新
+      // obj.gender = '男' // 该方式不会使界面更新界面
+      // user.gender = '男' // 该方式界面会更新，而且这个数据最终也添加到了obj对象中
+
+      // user对象或者obj对象中移除一个已经存在的属性，哪种方式会影响界面更新
+      // delete obj.age
+      // delete user.age
     }
 
     return {
-      count,
-      updateCount
+      user,
+      updateUser,
     }
   }
 });
