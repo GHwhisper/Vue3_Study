7549197c554448707fa102c6b05bd248dbcb7e0a
l.shuang <lsbright@qq.com>
2021-09-24 11:10:13 +0800

    vue2与vue3生命周期对比

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,24 @@
 <template>
-  <h2>计算属性和监视</h2>
-  <fieldset>
-    <legend>姓名操作</legend>
-    姓氏：<input type="text" placeholder="请输入姓氏" v-model="user.firstName"><br />
-    名字：<input type="text" placeholder="请输入名字" v-model="user.lastName"><br />
-  </fieldset>
-  <fieldset>
-    <legend>计算属性和监视</legend>
-    姓名：<input type="text" placeholder="显示姓名" v-model="fullName1"><br />
-    姓名：<input type="text" placeholder="显示姓名" v-model="fullName2"><br />
-    姓名：<input type="text" placeholder="显示姓名" v-model="fullName3"><br />
-  </fieldset>
+  <h2>App父级组件</h2>
+  <button @click="isShow = !isShow">切换显示</button>
+  <hr>
+  <child v-if="isShow"></child>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, computed, watch, ref, watchEffect } from 'vue'
+import Child from '@/components/Child.vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'App',
-
+  components: {
+    Child,
+  },
   setup() {
-    const user = reactive({
-      firstName: '东方',
-      lastName: '不败',
-    })
-
-    // vue3计算属性
-    // 计算属性的函数中如果只传入一个回调函数，表示的是get
-    // 返回一个Ref类型的对象
-    const fullName1 = computed(() => {
-      return user.firstName + '_' + user.lastName
-    })
-    // console.log(fullName1)
-
-    const fullName2 = computed({
-      get() {
-        return user.firstName + '_' + user.lastName
-      },
-      set(val: string) {
-        // console.log('===', val)
-        const names = val.split('_')
-        user.firstName = names[0]
-        user.lastName = names[1]
-      }
-    })
-
-    const fullName3 = ref('')
-    // 监视---监视指定的数据
-    // immediate 页面渲染时立即执行一次，deep 深度监视
-    watch(user, ({ firstName, lastName }) => {
-      fullName3.value = firstName + '_' + lastName
-    }, { immediate: true, deep: true })
-
-    // 监视，不需要配置immediate，本身就会默认执行一次
-    // watchEffect(() => {
-    //   fullName3.value = user.firstName + '_' + user.lastName
-    // })
-
-    // 监视 fullName3，改变 firstName 和 lastName
-    watchEffect(() => {
-      const names = fullName3.value.split('_')
-      user.firstName = names[0]
-      user.lastName = names[1]
-    })
-
-    // watch---可以监听多个数据
-    // 使用 watch 监听非响应式数据时，直接这样写，回调不会执行
-    // watch([user.firstName, user.lastName], () => {
-    //   console.log('===')
-    // })
-    // 改用如下写法，回调会执行
-    watch([() => user.firstName, () => user.lastName], () => {
-      console.log('===')
-    })
+    const isShow = ref(true)
 
     return {
-      user,
-      fullName1,
-      fullName2,
-      fullName3,
+      isShow,
     }
   }
 })
---
+++ src/components/Child.vue
@@ -0,0 +1,84 @@
+<template>
+  <h2>Child子级组件</h2>
+  <h4>msg: {{ msg }}</h4>
+  <button @click="update">更新数据</button>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted
+} from 'vue'
+
+export default defineComponent({
+  name: 'Child',
+  // vue2.x中的生命周期钩子
+  beforeCreate() {
+    console.log('2.x beforeCreate')
+  },
+  created() {
+    console.log('2.x created')
+  },
+  beforeMount() {
+    console.log('2.x beforeMount')
+  },
+  mounted() {
+    console.log('2.x mounted')
+  },
+  beforeUpdate() {
+    console.log('2.x beforeUpdate')
+  },
+  updated() {
+    console.log('2.x updated')
+  },
+  // vue2.x中的 beforeDestroy 和 destroyed 在 vue3 中分别改为 beforeUnmount 和 unmounted
+  beforeUnmount() {
+    console.log('beforeUnmount')
+  },
+  unmounted() {
+    console.log('unmounted')
+  },
+  setup() {
+    console.log('3.0 setup')
+
+    const msg = ref('abc')
+    const update = () => {
+      msg.value += '==='
+    }
+
+    onBeforeMount(() => {
+      console.log('3.0 onBeforeMount')
+    })
+    onMounted(() => {
+      console.log('3.0 onMounted')
+    })
+    onBeforeUpdate(() => {
+      console.log('3.0 onBeforeUpdate')
+    })
+    onUpdated(() => {
+      console.log('3.0 onUpdated')
+    })
+    onBeforeUnmount(() => {
+      console.log('3.0 onBeforeUnmount')
+    })
+    onUnmounted(() => {
+      console.log('3.0 onUnmounted')
+    })
+
+    return {
+      msg,
+      update,
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
