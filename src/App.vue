c893397e8474cb7d202c6a19c049da0b2af8d2f6
l.shuang <lsbright@qq.com>
2021-09-24 15:43:32 +0800

    自定义hook函数ajax请求，ts强化数据类型

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <template>
   <h2>自定义hook函数操作</h2>
   <h2>x: {{ x }}, y: {{ y }}</h2>
+  <h3 v-if="loading">正在加载中...</h3>
+  <h3 v-else-if="errorMsg">错误信息: {{ errorMsg }}</h3>
+  <ul v-else>
+    <li>id: {{ data.id }}</li>
+    <li>address: {{ data.address }}</li>
+    <li>distance: {{ data.distance }}</li>
+  </ul>
+  <hr>
+  <!--数组数据-->
+  <ul v-for="item in data" :key="item.id">
+    <li>id: {{ item.id }}</li>
+    <li>title: {{ item.title }}</li>
+    <li>price: {{ item.price }}</li>
+  </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, watch } from 'vue'
 import useMousePosition from '@/hook/useMousePosition'
+import useRequest from '@/hook/useRequest'
+
+// 定义接口，约束对象的类型
+interface IAddressData {
+  id: number,
+  address: string,
+  distance: string
+}
+
+interface IProductsData {
+  id: string,
+  title: string,
+  price: number
+}
 
 export default defineComponent({
   name: 'App',
@@ -13,9 +41,23 @@ export default defineComponent({
   setup() {
     const { x, y } = useMousePosition()
 
+    // 发送请求
+    // const { loading, data, errorMsg } = useRequest<IAddressData>('/data/address.json') // 获取对象数据
+    const { loading, data, errorMsg } = useRequest<IProductsData[]>('/data/products.json') // 获取数组数据
+
+    // 监视
+    watch(data, () => {
+      if (data.value) {
+        console.log(data.value.length)
+      }
+    })
+
     return {
       x,
       y,
+      loading,
+      data,
+      errorMsg,
     }
   }
 })
