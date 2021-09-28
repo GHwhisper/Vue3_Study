1ac121234ed958f0f273a04ed129f24603be54b3
l.shuang <lsbright@qq.com>
2021-09-28 15:45:54 +0800

    Suspense组件的使用

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <h2>App父级组件</h2>
-  <hr>
-  <modal-button></modal-button>
+  <h2>App父级组件：Suspense组件的使用</h2>
+  <suspense>
+    <template #default>
+      <!-- 异步组件 -->
+<!--      <async-component></async-component>-->
+      <async-address></async-address>
+    </template>
+    <template v-slot:fallback>
+      <!-- loading的内容 -->
+      <h2>Loading...</h2>
+    </template>
+  </suspense>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import ModalButton from '@/ModalButton.vue'
+import { defineComponent, defineAsyncComponent } from 'vue'
+// 引入组件：静态引入和动态引入
+
+// Vue2 中的动态引入组件写法：（在 Vue3 中该写法不行）
+// const AsyncComponent = () => import('@/AsyncComponent.vue')
+
+// Vue3 中动态引入组件写法：
+// const AsyncComponent = defineAsyncComponent(() => import('@/AsyncComponent.vue'))
+
+// 静态引入
+// import AsyncComponent from '@/AsyncComponent.vue'
+import AsyncAddress from '@/AsyncAddress.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    ModalButton
+    // AsyncComponent,
+    AsyncAddress,
   }
 })
 </script>
